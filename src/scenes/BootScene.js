f6f6993da3caa65be481149961334b2995e8ddca
wnstn4896 <56909907+wnstn4896@users.noreply.github.com>
2025-07-03 17:48:59 +0900

    Update BootScene.js

--- src/scenes/BootScene.js
+++ src/scenes/BootScene.js
@@ -15,6 +15,8 @@ export class BootScene extends Phaser.Scene {
 
         this.load.image('testField', `${img}/testField.png`);
 
+        this.load.image('attack_effect', `${img}/attack_effect.png`);
+
         
         // 모바일용 버튼 이미지 정의
         this.load.image('left_btn', `${img}/left_button.png`);
@@ -26,18 +28,27 @@ export class BootScene extends Phaser.Scene {
         for (let i=1; i <= 6; i++) {
             this.load.image('Reed_walk' + i, `${img}/Reed_walk` + i + '.png');
             this.load.image('Aster_walk' + i, `${img}/Aster_walk` + i + '.png');
+            this.load.image('Tuto_walk' + i, `${img}/Tuto_walk` + i + '.png');
         }
         for (let i=1; i<=3; i++){
             this.load.image('Reed_jump' + i, `${img}/Reed_jump` + i + '.png');
             this.load.image('Aster_jump' + i, `${img}/Aster_jump` + i + '.png');
             this.load.image('Reed_attack' + i, `${img}/Reed_attack` + i + '.png');
+            this.load.image('Aster_attack' + i, `${img}/Aster_attack` + i + '.png');
         }
 
         // 대사 메시지 JSON 로드
         this.load.json('PrologueDialogues', `${msg}/Prologue.json`);
 
         // 사운드 로드
-        this.load.audio('prologueBGM', [`${sound}/prologue.mp3`]);  
+        this.load.audio('prologueBGM', [`${sound}/prologue.mp3`]);
+
+        // 효과음
+        this.load.audio('sfx_attack', [`${sound}/sfx_attack.wav`]);
+        this.load.audio('sfx_jump', [`${sound}/sfx_jump.wav`]);
+        this.load.audio('sfx_jump2', [`${sound}/sfx_jump2.mp3`]);
+        this.load.audio('sfx_jumpattack', [`${sound}/sfx_jumpattack.ogg`]);
+        this.load.audio('Tuto_damaged', [`${sound}/Tuto_damaged.wav`]);
     }
 
     create() {
